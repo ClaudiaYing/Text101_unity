48510b22054b34a0b664e65bb8a37fd9b0e946cf
Claudia Ying <claudiaying@ClaudiadeMacBook-Pro.local>
2020-01-22 01:50:19 -0500

    initial commit

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14,10)][SerializeField] string stroyText;
+    [SerializeField] State[] nextStates;
+
+
+    public string GetStateStory()
+    {
+        return stroyText;
+    }
+
+    public State[] GetNextState()
+    {
+        return nextStates;
+    }
+   
+}
